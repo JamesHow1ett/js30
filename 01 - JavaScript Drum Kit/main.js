6c1f129da1270bf5324f643f35fb6f11ff575c8e
oleksandr.skorokhod <oleksandr.skorokhod@aimprosoft.com>
2021-01-19 13:50:48 +0200

    first commit

---
+++ 01 - JavaScript Drum Kit/main.js
@@ -0,0 +1,21 @@
+const playSound = (event) => {
+  const audio = document.querySelector(`audio[data-key="${event.code}"]`);
+  const key  = document.querySelector(`.key[data-key="${event.code}"]`);
+
+  if (!audio) { return; }
+  
+  audio.currentTime = 0;
+  audio.play();
+
+  key.classList.add('playing');
+}
+
+function removeTransition(event) {
+  if (event.propertyName !== 'transform') { return; }
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
