e9bf77fc304b64ab35e80067cefaab1e40f2d822
oleksandr.skorokhod <oleksandr.skorokhod@aimprosoft.com>
2021-01-21 16:55:03 +0200

    done 08
    Signed-off-by: oleksandr.skorokhod <oleksandr.skorokhod@aimprosoft.com>

---
+++ 08 - Fun with HTML5 Canvas/main.js
@@ -0,0 +1,43 @@
+const canvas = document.getElementById('draw');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.strokeStyle = '#bada55';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 100;
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+let direction = true;
+
+const draw = (event) => {
+  if (!isDrawing) { return; }
+
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  ctx.beginPath();
+  // start from
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(event.offsetX, event.offsetY);
+  ctx.stroke();
+  // go to
+  [lastX, lastY] = [event.offsetX, event.offsetY];
+  // hue staff
+  hue++;
+  if (hue > 359) { hue = 0; }
+  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) { direction = !direction; }
+  direction ? ctx.lineWidth++ : ctx.lineWidth--;
+
+}
+
+canvas.addEventListener('mousedown', (event) => {
+  isDrawing = true;
+  [lastX, lastY] = [event.offsetX, event.offsetY];
+});
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
